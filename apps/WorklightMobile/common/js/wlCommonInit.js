65f13179f72b2f3a30fd2633240b48366094f058
nurdin <lowyat_net@yahoo.com>
2014-01-15 15:07:44 +0800

    add bin files

--- apps/WorklightMobile/common/js/wlCommonInit.js
+++ apps/WorklightMobile/common/js/wlCommonInit.js
@@ -7,27 +7,37 @@
 */
 
 function wlCommonInit(){
+	loadFeeds();
+}
 
-	/*
-	 * Application is started in offline mode as defined by a connectOnStartup property in initOptions.js file.
-	 * In order to begin communicating with Worklight Server you need to either:
-	 * 
-	 * 1. Change connectOnStartup property in initOptions.js to true. 
-	 *    This will make Worklight framework automatically attempt to connect to Worklight Server as a part of application start-up.
-	 *    Keep in mind - this may increase application start-up time.
-	 *    
-	 * 2. Use WL.Client.connect() API once connectivity to a Worklight Server is required. 
-	 *    This API needs to be called only once, before any other WL.Client methods that communicate with the Worklight Server.
-	 *    Don't forget to specify and implement onSuccess and onFailure callback functions for WL.Client.connect(), e.g:
-	 *    
-	 *    WL.Client.connect({
-	 *    		onSuccess: onConnectSuccess,
-	 *    		onFailure: onConnectFailure
-	 *    });
-	 *     
-	 */
+function loadFeeds(){
+	var email = "sale@tshirtshop.com";
+	var password ="aaaa1111";
+	var invocationData = {
+			adapter : 'UserId',
+			procedure : 'getUserId',
+			parameters : [email,password]
+		};
 	
-	
-	// Common initialization code goes here
+	WL.Client.invokeProcedure(invocationData,{
+		onSuccess : loadFeedsSuccess,
+		onFailure : loadFeedsFailure
+	});
+}
 
+function loadFeedsSuccess(result){
+	WL.Logger.debug("Feed retrieve success");
+	if (result.invocationResult.tbl_merchant.length>0) 
+		alert("success");
+	else 
+		loadFeedsFailure();
+}
+
+function loadFeedsFailure(result){
+	WL.Logger.error("Feed retrieve failure");
+	WL.SimpleDialog.show("EngadgetReader", "Cannot retrieve feed. Please check your internet connectivity.", 
+	[{
+		text : 'Reload App',
+		handler : WL.Client.reloadApp 
+	}]);
 }
